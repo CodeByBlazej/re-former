adac8c3297a27a5a40c659026e64a8beceb7b97c
CodeByBlazej <codebyblazej@gmail.com>
2025-10-22 17:02:00 +0100

    add strong params method and tweak form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], email_confirmation: params[:email_confirmation], password: params[:password], password_confirmation: params[:password_confirmation])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
     else
-      render :new, status: :unprocessable_entity
+      # render :new, status: :unprocessable_entity
     end
+  end
+
+  def user_params
+    params.expect(user: [ :username, :email, :email_confirmation, :password, :password_confirmation ])
   end
 end
