1c40a193a730fff4bb8a09e4ba9425a1af550772
CodeByBlazej <codebyblazej@gmail.com>
2025-10-26 13:42:32 +0000

    add new route, method and errors in form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.expect(user: [ :username, :email, :email_confirmation, :password, :password_confirmation ])
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
-  validates :email, confirmation: true, uniqueness: true
-  validates :email_confirmation, presence: true
+  validates :email, confirmation: true, uniqueness: true, presence: true
+  validates :email_confirmation, presence: true, if: :will_save_change_to_email?
 
-  validates :password, confirmation: true, length: { in: 6..20 }
-  validates :password_confirmation, presence: true
+  validates :password, presence: true, on: :create
+  validates :password, confirmation: true, length: { in: 6..20 }, allow_blank: true
+  validates :password_confirmation, presence: true, if: -> { password.present? }
 end
